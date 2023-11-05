e2bb3af683a63e3138a47a9beef26148eea11e26
Began <beganbajrami@gmail.com>
2023-11-05 18:30:59 +0100

    Update .config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -194,8 +194,8 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- Move to previous/next
-map('n', '<A-k>', '<Cmd>BufferPrevious<CR>', opts)
-map('n', '<A-j>', '<Cmd>BufferNext<CR>', opts)
+map('n', '<A-j>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<A-k>', '<Cmd>BufferNext<CR>', opts)
 -- Re-order to previous/next
 map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
 map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
