6479cf446ea21855a29d61819ce5a07b56624ad8
Began <beganbajrami@gmail.com>
2023-11-05 18:29:13 +0100

    Update .config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -192,9 +192,10 @@ vim.keymap.set('n', '<leader>sr', builtin.resume,       { desc = '[S]earch [R]es
 
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
+
 -- Move to previous/next
-map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
-map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+map('n', '<A-k>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<A-j>', '<Cmd>BufferNext<CR>', opts)
 -- Re-order to previous/next
 map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
 map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
@@ -212,7 +213,7 @@ map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
 -- Pin/unpin buffer
 map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
 -- Close buffer
-map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+map('n', '<A-w>', '<Cmd>BufferClose<CR>', opts)
 
 -- Custom mappings
 local api = require "nvim-tree.api"
